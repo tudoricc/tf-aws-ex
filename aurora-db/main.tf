f01c28de4ddc6c6bb42d543d008cb56dafe33f0a
tudoricc <vasiletudor30@yahoo.com>
2023-09-17 21:34:52 +0300

    Adding the aurora db module

--- aurora-db/main.tf
+++ aurora-db/main.tf
@@ -5,7 +5,7 @@ variable "environment" {}
 provider "aws" {
   region = var.region_aws
 }
-
+data "aws_availability_zones" "available" {}
 # we created the vpc in another tf module so we can use the data from the remote state here rather than hardcoding it
 data "terraform_remote_state" "vpc" {
   backend = "local"
@@ -33,7 +33,7 @@ data "terraform_remote_state" "iam-ecs-asg" {
 locals {
   #Because we don't want this hardcoded we are retrieving hte VPC id from the remote state of the vpc module
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
-
+  #azs  = data.terraform_remote_state.vpc.outputs.az-available
   #We are retrieving the private db subnnets id from the remote state of the vpc moodule
   db_subnets = data.terraform_remote_state.vpc.outputs.db_subnets_ids
   
@@ -41,22 +41,27 @@ locals {
   #iam role
 
   aurora-cluster-name = "sonar-aurora-${var.region_aws}-${var.environment}"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
 
 ################################################################################
 # RDS Aurora Module
 ################################################################################
+resource "random_password" "master" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+#because I am cheap and I don't want extra costs  using servlerless so if it's not used we don't pay
+module "aurora_postgresql" {
+  source = "terraform-aws-modules/rds-aurora/aws"
 
-module "aurora" {
-  source = "../../"
-
-  name            = local.aurora-cluster-name
-  engine          = "aurora-postgresql"
-  engine_version  = "14.5"
-  instance_class  = "db.serverless"
-  instances       = { 1 = {} }
-  master_username = "root"
+  name              = "${local.aurora-cluster-name}-postgresql"
+  engine            = "aurora-postgresql"
+  engine_mode       = "serverless"
+  storage_encrypted = true
+  master_username   = "root"
 
   vpc_id               = local.vpc_id
   db_subnet_group_name = local.db_subnets
@@ -66,27 +71,24 @@ module "aurora" {
     }
   }
 
-  autoscaling_enabled      = true
-  autoscaling_min_capacity = 1
-  autoscaling_max_capacity = 5
+  manage_master_user_password = false
+  master_password             = random_password.master.result
 
-  monitoring_interval           = 60
-  iam_role_name                 = "${local.aurora-cluster-name}-monitor"
-  iam_role_use_name_prefix      = true
-  iam_role_description          = "${local.aurora-cluster-name} RDS enhanced monitoring IAM role"
-  iam_role_path                 = "/autoscaling/"
-  iam_role_max_session_duration = 7200
+  monitoring_interval = 60
 
   apply_immediately   = true
   skip_final_snapshot = true
 
-  enabled_cloudwatch_logs_exports = ["postgresql"]
+  # enabled_cloudwatch_logs_exports = # NOT SUPPORTED
 
-  tags = local.tags
+  scaling_configuration = {
+    auto_pause               = true
+    min_capacity             = 2
+    max_capacity             = 16
+    seconds_until_auto_pause = 300
+    timeout_action           = "ForceApplyCapacityChange"
+  }
+
+
 }
 
-module "disabled_aurora" {
-  source = "../../"
-
-  create = false
-}
--- aurora-db/vars/eu-west-1.tfvars
+++ aurora-db/vars/eu-west-1.tfvars
@@ -1,2 +1,3 @@
 region_aws = "eu-west-1"
 environment = "dev"
+#azs = ["eu-west-1a","eu-west-1b","eu-west-1c"]
